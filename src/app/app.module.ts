5bd7a203ec95d60dd15deb0e3013bf7a629bcb39
Oskar Pozdnakovs <oskars.pozdnakovs@groupcdg.com>
2023-09-06 18:38:27 +0100

    transfer provided single page application code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CountTileComponent } from './components/count-tile/count-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    CountTileComponent
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
     HttpClientModule
