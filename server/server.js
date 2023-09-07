1c17d2ed3a4bbdd15d7c4ceea5085790b78a6d06
Oskar Pozdnakovs <oskars.pozdnakovs@groupcdg.com>
2023-09-07 10:34:22 +0100

    add mocked GET funds endpoint

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 
 
-app.get("/api/:id", (request, response) => {
-  response.send({
-      id: request.params.id
-    }
+app.get("/api/funds", (request, response) => {
+  response.send([
+      {id: "123", name: "Fund A"},
+      {id: "124", name: "Fund B"}
+    ]
+  );
+});
+
+app.get("/api/partnership-transfer/search", (request, response) => {
+  response.send([
+      // todo
+    ]
   );
 });
 
